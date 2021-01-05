482d6707545927ac2abf349e5d5e33c8d549032f
Beastyy99 <seyidave99@gmail.com>
2021-01-05 02:39:56 +0100

    slight update

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ const itemsSchema = new mongoose.Schema({
   name:String
 })
 
-const Item = mongoose.model('Item',itemsSchema);
+const Item = mongoose.model("Item",itemsSchema);
 const item1 = new Item({
   name: "welcome to your to-do-list!"
-})
+});
 const item2 = new Item({
   name: "Hit the + button to add a new item"
-})
+});
 const item3 = new Item({
   name: "<-- Hit this to delete an item"
-})
+});
 const defaultItems = [item1,item2,item3];
 const listSchema = {
   name:String,
